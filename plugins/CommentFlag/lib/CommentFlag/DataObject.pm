1b11eb0132223a1cedc5079515e715ba66edc50f
Mike Thomsen <mikethomsen@Mike-Thomsens-MacBook-Pro.local>
2009-09-18 21:43:30 -0400

    The flag object is now complete?

---
+++ plugins/CommentFlag/lib/CommentFlag/DataObject.pm
@@ -0,0 +1,20 @@
+package CommentFlag::DataObject;
+
+use strict;
+use base qw(MT::Object);
+
+__PACKAGE__->install_properties({
+	column_defs => {
+		id			=>	'integer autoincrement',
+		comment_id		=>	'integer not null',
+		reporter_name		=>	'string(64)',
+		reporter_email		=>	'string(64)',
+		reporter_id		=>	'integer not null',
+		explanation		=>	'text'
+	}, datasource => 'comment_flag',
+	primary_key => 'id',
+	audit => 1
+});
+
+1;
+
